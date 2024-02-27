d3f2a5ba70e982e147bc786cbc55c32fa5b3e5d9
Vidit Tamrakar <vidit.tamrakar16@gmail.com>
2024-02-28 01:23:44 +0530

    files added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,134 @@
-import Image from "next/image";
+"use client"
 import styles from "./page.module.css";
+import AppleLogo from "./applePixels.png"
+import Monitor from "./oldMonitor.png"
+import useInterval from "./useInterval"
+import { useState,useRef,useEffect } from "react";
+
+const canvasX = 1000
+const canvasY = 1000
+const initialSnake = [ [ 4, 10 ], [ 4, 10 ] ]
+const initialApple = [ 14, 10 ]
+const scale = 50
+const timeDelay = 100
+
 
 export default function Home() {
+
+  const canvasRef = useRef<HTMLCanvasElement | null>(null)
+	const [ snake, setSnake ] = useState(initialSnake)
+	const [ apple, setApple ] = useState(initialApple)
+	const [ direction, setDirection ] = useState([ 0, -1 ])
+	const [ delay, setDelay ] = useState<number | null>(null)
+	const [ gameOver, setGameOver ] = useState(false)
+	const [ score, setScore ] = useState(0)
+	const [ high, sethighScore ] = useState(0)
+
+	useInterval(() => runGame(), delay)
+
+	useEffect(
+		() => {
+			let fruit =  document.getElementById("fruit") as HTMLCanvasElement
+			if (canvasRef.current) {
+				const canvas = canvasRef.current
+				const ctx = canvas.getContext("2d")
+				if (ctx) {
+					ctx.setTransform(scale, 0, 0, scale, 0, 0)
+					ctx.clearRect(0, 0, window.innerWidth, window.innerHeight)
+					ctx.fillStyle = "#a3d001"
+          
+					snake.forEach(([ x, y ]) => ctx.fillRect(x, y, 1, 1))
+					ctx.drawImage(fruit, apple[0], apple[1], 1, 1)
+				}
+			}
+		},
+		[ snake, apple, gameOver ]
+	)
+
+	function handleSetScore() {
+		if (score > high) {
+			sethighScore(score)
+		}
+	}
+
+	function play() {
+		setSnake(initialSnake)
+		setApple(initialApple)
+		setDirection([ 1, 0 ])
+		setDelay(timeDelay)
+		setScore(0)
+		setGameOver(false)
+	}
+
+	function checkCollision(head: number[]) {
+		for (let i = 0; i < head.length; i++) {
+			if (head[i] < 0 || head[i] * scale >= canvasX) return true
+		}
+		for (const s of snake) {
+			if (head[0] === s[0] && head[1] === s[1]) return true
+		}
+		return false
+	}
+
+	function appleAte(newSnake: number[][]) {
+		let coord = apple.map(() => Math.floor(Math.random() * canvasX / scale))
+		if (newSnake[0][0] === apple[0] && newSnake[0][1] === apple[1]) {
+			let newApple = coord
+			setScore(score + 1)
+			setApple(newApple)
+			return true
+		}
+		return false
+	}
+
+	function runGame() {
+		const newSnake = [ ...snake ]
+		const newSnakeHead = [ newSnake[0][0] + direction[0], newSnake[0][1] + direction[1] ]
+		newSnake.unshift(newSnakeHead)
+		if (checkCollision(newSnakeHead)) {
+			setDelay(null)
+			setGameOver(true)
+			// handleSetScore()
+		}
+		if (!appleAte(newSnake)) {
+			newSnake.pop()
+		}
+		setSnake(newSnake)
+	}
+
+	function changeDirection(e: React.KeyboardEvent<HTMLDivElement>) {
+		switch (e.key) {
+			case "ArrowLeft":
+				setDirection([ -1, 0 ])
+				break
+			case "ArrowUp":
+				setDirection([ 0, -1 ])
+				break
+			case "ArrowRight":
+				setDirection([ 1, 0 ])
+				break
+			case "ArrowDown":
+				setDirection([ 0, 1 ])
+				break
+		}
+	}
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="home">
+      	<div onKeyDown={(e) => changeDirection(e)}>
+			<img id="fruit" src="/images/applePixels.png" alt="fruit" width="30" />
+			{/* <img src="/images/oldMonitor.png" alt="fruit" width="4000" className="monitor" /> */}
+			<canvas className="playArea" ref={canvasRef} width={`${canvasX}px`} height={`${canvasY}px`} />
+			{gameOver && <div className="gameOver">Game Over</div>}
+			<button onClick={play} className="playButton">
+				Play
+			</button>
+			<div className="scoreBox">
+				<h2>Score: {score}</h2>
+				
+			</div>
+		</div>
+     
     </main>
   );
 }
